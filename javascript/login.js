aadd31a60df555001fc60a50d8971401764fb0ff
tmdeh <yus174113@gmail.com>
2021-11-07 21:29:54 +0900

    jquery

---
+++ javascript/login.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('button').click(function () { 
+        var password = $('input[name=password]').val();
+        console.log(password);
+    });
+});
+
+
--- public/javascript/main.js
+++
@@ -1,8 +0,0 @@
-const { io } = require("socket.io-client");
-const socket = io();
-
-
-
-socket.on('connection', (socket) => {
-    console.log(socket.id);
-})
